517afc372d7426fb312d20b2882e4e8e9e382383
Maarten Merken <merken@pop-os.localdomain>
2020-11-12 11:57:16 +0100

    todo test this

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PlatformTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"AppContext.BaseDirectory {AppContext.BaseDirectory}");
+            Console.WriteLine($"RuntimeInformation.RuntimeIdentifier {RuntimeInformation.RuntimeIdentifier}");
+            Console.WriteLine($"RuntimeInformation.OSDescription {RuntimeInformation.OSDescription}");
+            Console.WriteLine($"RuntimeInformation.ProcessArchitecture {RuntimeInformation.ProcessArchitecture}");
+            Console.WriteLine($"RuntimeInformation.OSArchitecture {RuntimeInformation.OSArchitecture}");
+            // Console.WriteLine($"Environment.OSDescription {Environment.OSDescription}");
+            Console.WriteLine( $"{Microsoft.DotNet.PlatformAbstractions.RuntimeEnvironment.OperatingSystemPlatform.ToString().ToLower()}-{Microsoft.DotNet.PlatformAbstractions.RuntimeEnvironment.RuntimeArchitecture}");
+        }
+    }
+}
